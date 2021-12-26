381a6a9f3ebd0bfee4f28123a369bc8b8f9b6d0e
xCalonso <96618051+xCalonso@users.noreply.github.com>
2021-12-26 17:22:59 +0100

    Actualizado

--- index.js
+++ index.js
@@ -12,6 +12,23 @@ const usuarios = require('./backend/sql/usuarios')
 const juegos = require('./backend/sql/juegos')
 
 
+const app = express()
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+app.use(bodyParser.json())
+
+
+var cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+
 const oracledb = require('oracledb');
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
@@ -30,6 +47,7 @@ async function ConectarBD() {
     console.error(err);
   } finally {
     if (connection) {
+      console.log("Conexion establecida correctamente!!")
       try {
         await connection.close();
       } catch (err) {
@@ -41,3 +59,21 @@ async function ConectarBD() {
 
 ConectarBD();
 
+
+app.use(express.static('frontend/dist'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/frontend/dist/index.html');
+});
+
+
+app.listen(port, () => {
+  console.log(`Backend funcionando en http://localhost:${port}`)
+})
+
+process.on('error', function (err) {
+  console.log(err);
+});
+
+process.on('exit', function () {
+  connection.end();
+});
