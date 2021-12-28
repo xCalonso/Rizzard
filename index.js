c111ee425a2c03a9f6b34e7bd3da787d770b5369
xCalonso <96618051+xCalonso@users.noreply.github.com>
2021-12-28 12:35:46 +0100

    Actualizado

--- index.js
+++ index.js
@@ -28,15 +28,75 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
+/*const pg = require('pg');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+
+async function ConectarBD() {  
+  try{
+    let cliente = new pg.Client({
+      user: "cjqcyjcicnlgid",
+      password: "9b518c96ec8f3403b6bb2513ea813c91f70e523f4d58fe36b051dceb97fabd52",
+      database: "dc97m4rbk5jl0p",
+      port: 5432,
+      host: "ec2-3-217-216-13.compute-1.amazonaws.com",
+      ssl: true
+    })
+    await cliente.connect()
+    console.log("Conexion establecida!!")
+    
+    cliente.query("CREATE TABLE Prueba", (err, res) => {
+    	if (err) {
+    		console.log(err)
+    	} else {
+    		console.log(res.rows)
+    	}
+    })
+    
+    cliente.query("DESCRIBE Prueba", (err, res) => {
+    if (err) {
+      console.log(err.stack)
+    } else {
+      console.log(res.rows)
+    }
+})
+  } catch (err) {
+    console.log(err);
+  }
+}*/
+
+const oracledb = require('oracledb');
+oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+
 async function ConectarBD() {
 
+  let connection;
+
+  try {
+    connection = await oracledb.getConnection( {
+      user          : "x7160417",
+      password      : "x7160417",
+      connectString : "oracle0.ugr.es:1521/practbd.oracle0.ugr.es"
+    });
+
+  } catch (err) {
+    console.error(err);
+  } finally {
+    if (connection) {
+      try {
+        console.log("Conexion establecida!!")
+        await connection.close();
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
 }
 
+ConectarBD();
 
-
-app.use(express.static('frontend/dist'));
+app.use(express.static('frontend/dist'))
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/frontend/dist/index.html');
+  res.sendFile(__dirname + '/frontend/dist/index.html')
 });
 
 
